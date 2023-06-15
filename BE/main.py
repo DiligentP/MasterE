b758f59b32b3f17717efd527c6d01ae08741d5aa
root <root@Dev-Park.com>
2023-06-15 20:47:37 +0000

    Add CORS Setting

--- BE/main.py
+++ BE/main.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select, func
 from databases import Database
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
